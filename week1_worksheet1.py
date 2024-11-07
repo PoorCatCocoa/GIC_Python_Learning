b02795140b559857ac24f592936766b9517be487
PoorCatCocoa <poorcatcocoa@gmail.com>
2024-11-07 14:53:28 +0000

    upload test

---
+++ week1_worksheet1.py
@@ -0,0 +1,51 @@
+# This is a program to print.
+print ("This is my first Python program")
+
+a = 2+3+5
+b = 2/3 - 1/4
+c = 25*3
+d = 7/3
+e = 14//3
+f = 7%3
+g = 2 ** 4
+
+num = a+b+c+d+e+f+g
+print (num)
+
+apple = 5
+banana = 20
+pears = 10
+peaches = 7
+
+fruits = apple+banana+pears+peaches
+print (fruits)
+
+apple -= 4
+banana -= 15
+pears -= 6
+peaches -= 7
+
+fruits = apple+banana+pears+peaches
+print (fruits)
+
+
+
+i = 7 /4
+ii = 8 //2
+iii = 13 /24
+print (i, ii, iii)
+
+
+
+a = 4
+b = 2
+c = a %b
+print (c)
+
+
+
+my_name = input ("Please enter your name: ")
+print ("Welcome" +my_name)
+
+
+
